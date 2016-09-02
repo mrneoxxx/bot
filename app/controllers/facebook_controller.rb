4896aa05475e6302220e948c6eceda5da4a0c0ce
Oleksandr Kabarivskiy <oleksandr.kabarivskiy@gmail.com>
2016-09-02 14:56:35 +0300

    changes

--- app/controllers/facebook_controller.rb
+++ app/controllers/facebook_controller.rb
@@ -1,8 +1,35 @@
 class FacebookController < Messenger::MessengerController
 
-  def webhook
-    #logic here
+  def index
+    
+    p '!!!!!!!!!!!!!!!!!!!!!!!'
+    p fb_params
+    p '!!!!!!!!!!!!!!!!!!!!!!!'
+    
+    if fb_params.first_entry.callback.message?
+      Messenger::Client.send(
+        Messenger::Request.new(
+          Messenger::Elements::Text.new(text: "Echo: #{fb_params.first_entry.callback.text}"),
+          fb_params.first_entry.sender_id
+        )
+      )
+    end
+    
     render nothing: true, status: 200
   end
+  
+#  def webhook
+    #logic here
+    
+#    if fb_params.first_entry.callback.message?
+#      Messenger::Client.send(
+#        Messenger::Request.new(
+#          Messenger::Elements::Text.new(text: "Echo: #{fb_params.first_entry.callback.text}"),
+#          fb_params.first_entry.sender_id
+#        )
+#      )
+#    end
+#    render nothing: true, status: 200
+#  end
   
 end
--- config/routes.rb
+++ config/routes.rb
@@ -4,8 +4,20 @@
 #   resources :chat
 #   mount ActionCable.server => '/cable'
 # end
+Messenger::Engine.routes.draw do
+  root to: "messenger#validate"
+end
 Rails.application.routes.draw do
   root to: "index#index"
+#  resources :facebook
+#  namespace :facebook do
+#    root to: "#index"
+#    mount Messenger::Engine
+#  end
   mount Messenger::Engine, at: "/facebook"
+  post 'facebook', to: "facebook#index"
+  
+  
+#  post 'facebook/webhook', to: "messenger#webhook"
 end
   
